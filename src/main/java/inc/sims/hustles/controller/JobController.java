b39e39d7f1b3cb570e6b46df2e25779c00938695
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-20 23:04:28 +0200

    Display the job that has been posted.

--- src/main/java/inc/sims/hustles/controller/JobController.java
+++ src/main/java/inc/sims/hustles/controller/JobController.java
@@ -1,23 +1,27 @@
 package inc.sims.hustles.controller;
 
+import inc.sims.hustles.model.JobPost;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
 
-    @RequestMapping({"/", "home"})
+    @GetMapping({"/", "home"})
     public String home(){
         return "home";
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addJob(){
         return "addjob";
     }
 
-    @RequestMapping("handleForm")
-    public String handleForm(){
+    @PostMapping("handleForm")
+    public String handleForm(@ModelAttribute JobPost jobPost){
         return "success";
     }
 }

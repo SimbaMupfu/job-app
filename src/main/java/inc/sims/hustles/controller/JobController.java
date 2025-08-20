3726aad1b6d0b1d8d30f7914b8bd0f2606b8aaae
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-20 22:51:55 +0200

    Map urls in the JobController

---
+++ src/main/java/inc/sims/hustles/controller/JobController.java
@@ -0,0 +1,23 @@
+package inc.sims.hustles.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/", "home"})
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("addjob")
+    public String addJob(){
+        return "addjob";
+    }
+
+    @RequestMapping("handleForm")
+    public String handleForm(){
+        return "success";
+    }
+}

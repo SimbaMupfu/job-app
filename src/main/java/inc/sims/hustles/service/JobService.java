73a3ae337d62cc433ec47c243afad946e37cc73b
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-21 23:45:39 +0200

    Create Job Service class

---
+++ src/main/java/inc/sims/hustles/service/JobService.java
@@ -0,0 +1,16 @@
+package inc.sims.hustles.service;
+
+import inc.sims.hustles.model.JobPost;
+
+import java.util.List;
+
+public class JobService {
+
+    public void addJob() {
+
+    }
+
+    public List<JobPost> getAllJobs(){
+
+    }
+}

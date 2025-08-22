b1ebc854cdbaf6fa8a2375c98013021211be34a4
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-22 06:37:13 +0200

    Add functionality to JobService

--- src/main/java/inc/sims/hustles/repository/JobRepository.java
+++ src/main/java/inc/sims/hustles/repository/JobRepository.java
@@ -1,11 +1,13 @@
 package inc.sims.hustles.repository;
 
 import inc.sims.hustles.model.JobPost;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Repository
 public class JobRepository {
     List<JobPost> jobs = new ArrayList<>(Arrays.asList(
             new JobPost(1, "Java Developer", "Must have good experience in core Java and advanced Java", 2,

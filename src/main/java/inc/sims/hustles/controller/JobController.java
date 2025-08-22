1d2cba1313a72e25b9ea1921b639f26b4bd50792
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-22 07:00:26 +0200

    Add functionality to view all jobs.

--- src/main/java/inc/sims/hustles/controller/JobController.java
+++ src/main/java/inc/sims/hustles/controller/JobController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JobController {
@@ -29,5 +30,12 @@ public class JobController {
     public String handleForm(@ModelAttribute JobPost jobPost){
         jobService.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public ModelAndView viewAllJobs(ModelAndView mv){
+        mv.addObject("jobPosts", jobService.getAllJobs());
+        mv.setViewName("viewalljobs");
+        return mv;
     }
 }
